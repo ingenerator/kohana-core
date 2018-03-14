65a60c50bb3a9c2c673256a369ba67a3cb0caef7
Andrew Coulton <andrew@ingenerator.com>
2018-03-14 11:17:05 +0000

    Run minion tests as part of the main kohana build
    Add a matrix to set KOHARNESS_ALL_MODULES and run a build with
    and without the optional modules.

--- koharness.php
+++ koharness.php
@@ -1,8 +1,13 @@
 <?php
 // Configuration for koharness - builds a standalone skeleton Kohana app for running unit tests
-return array(
-	'modules' => array(
-		'unittest' => __DIR__ . '/vendor/kohana/unittest'
-	),
-	'syspath' => __DIR__,
-);
+$base = [
+    'modules' => [
+        'unittest' => __DIR__.'/vendor/kohana/unittest',
+    ],
+    'syspath' => __DIR__,
+];
+
+if (getenv('KOHARNESS_ALL_MODULES')) {
+    $base['modules']['minion'] = __DIR__.'/modules/minion';
+}
+return $base;
