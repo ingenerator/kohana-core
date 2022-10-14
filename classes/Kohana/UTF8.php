8d88d46c41d5dd39b1b30b37c17cf8ef939bfe80
craig410 <craig@netchaos.net>
2022-10-14 14:06:28 +0100

    Fix strcasecomp to match underlying stdlib strcasecomp

--- classes/Kohana/UTF8.php
+++ classes/Kohana/UTF8.php
@@ -346,7 +346,15 @@ class Kohana_UTF8 {
 			UTF8::$called[__FUNCTION__] = TRUE;
 		}
 
-		return _strcasecmp($str1, $str2);
+		$comp = _strcasecmp($str1, $str2);
+
+		if ($comp > 0) {
+			return 1;
+		} elseif ($comp < 0) {
+			return -1;
+		}
+
+		return 0;
 	}
 
 	/**
